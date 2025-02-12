0d83740e06c53cee776b3061376401546d336c89
niebieskie <73031859+niebieskie@users.noreply.github.com>
2025-02-12 12:10:42 +0100

    Create level288.js

---
+++ src/utils/levels/level288.js
@@ -0,0 +1,120 @@
+
+import { altoMain, anakiwa, bittersweet, celadon, chardonnay, coldPurple, lightOrchid, nomad, saharaSand } from "../colors";
+
+const level288 = {
+  size: 9,
+  colorRegions: [
+  [
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "A",
+    "B"
+  ],
+  [
+    "A",
+    "C",
+    "C",
+    "C",
+    "C",
+    "C",
+    "D",
+    "D",
+    "B"
+  ],
+  [
+    "A",
+    "F",
+    "C",
+    "H",
+    "H",
+    "H",
+    "H",
+    "D",
+    "B"
+  ],
+  [
+    "A",
+    "F",
+    "F",
+    "H",
+    "G",
+    "H",
+    "H",
+    "I",
+    "B"
+  ],
+  [
+    "A",
+    "F",
+    "B",
+    "B",
+    "G",
+    "G",
+    "H",
+    "I",
+    "B"
+  ],
+  [
+    "A",
+    "F",
+    "B",
+    "B",
+    "B",
+    "E",
+    "H",
+    "I",
+    "B"
+  ],
+  [
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "E",
+    "I",
+    "I",
+    "B"
+  ],
+  [
+    "B",
+    "B",
+    "B",
+    "E",
+    "E",
+    "E",
+    "E",
+    "I",
+    "B"
+  ],
+  [
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B",
+    "B"
+  ]
+],
+  regionColors: {
+    A: nomad,
+    B: chardonnay,
+    C: anakiwa,
+    D: celadon,
+    E: altoMain,
+    F: bittersweet,
+    G: saharaSand,
+    H: coldPurple,
+    I: lightOrchid
+  }
+};
+
+export default level288;
